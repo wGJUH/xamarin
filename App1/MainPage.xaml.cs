7c56d53e19567cc93eb6434399eec7361beabd18
vladislav.kipnis <vladislav.kipnis.work@yandex.ru>
2019-06-16 01:29:55 +0300

    remove all other logic from main page

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -21,7 +21,6 @@ namespace App1
             InitializeComponent();
             Title = "MAIN PAGE";
             this.FindByName<StackLayout>("stack").BackgroundColor = Color.Aquamarine;
-            if (NEED_POPULATE) populateItems();
             btn_first.Clicked += datePickerPageOpen;
             btn_second.Clicked += onNextPageClicked;
             btn_third.Clicked += onStepperPageOpenClicked;
@@ -40,61 +39,12 @@ namespace App1
 
         private async void onNextPageClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new DatePickerPage());
-        }
-
-        private void onSecondClicked(object sender, EventArgs e)
-        {
-            showTimePicker();
-        }
-
-        private void showTimePicker()
-        {
-            TimePicker timePicker = new TimePicker
-            {
-                Time = new TimeSpan(17, 0, 0)
-            };
-            addView(timePicker);
-        }
-
-        private void addView(View item)
-        {
-            this.FindByName<StackLayout>("stack").Children.Add(item);
-        }
-
-        private void populateItems()
-        {
-            for (int i = 1; i <= 5; i++)
-            {
-                stack.Children.Add(
-                   new Label
-                   {
-                       Text = "position: " + i,
-                       FontSize = 14 * i,
-                       HorizontalOptions = LayoutOptions.StartAndExpand,
-                       HorizontalTextAlignment = TextAlignment.Start,
-                       TextColor = Color.Blue
-                   });
-            }
+            await Navigation.PushAsync(new StartPage());
         }
 
         private async void datePickerPageOpen(Object sender, EventArgs e)
         {
             await Navigation.PushAsync(new DatePickerPage());
-        }
-
-        private void onButtonClicked(object sender, EventArgs e)
-        {
-            if ((sender as Button).Id == btn_first.Id)
-            {
-                btn_first.Text = "second";
-                btn_second.Text = "first";
-            }
-            else
-            {
-                btn_first.Text = "first";
-                btn_second.Text = "second";
-            }
         }
     }
 }
