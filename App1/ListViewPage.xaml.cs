66bf882383e4a740c03ad03a247ea412e7fc59f2
vladislav.kipnis <vladislav.kipnis.work@yandex.ru>
2019-06-28 14:24:02 +0300

    add logic to generate phones in time

--- App1/ListViewPage.xaml.cs
+++ App1/ListViewPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,27 +10,69 @@ using Xamarin.Forms.Xaml;
 
 namespace App1
 {
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListViewPage : ContentPage
     {
+
+        public TextCell textCell = new TextCell { };
+        private ObservableCollection<Phone> phones = new ObservableCollection<Phone>();
+
+
         public ListViewPage()
         {
             InitializeComponent();
             Title = "List View Page";
-
-            randomListView.ItemsSource = PopulateListItems();
+            
+            randomListView.HasUnevenRows = true;
+            randomListView.ItemTemplate = new DataTemplate(DataTemplateViewCell);
+            randomListView.ItemsSource = phones;
+            PopulateListItems();
         }
 
-        private List<String> PopulateListItems()
+
+        private ViewCell DataTemplateViewCell()
         {
-            var strings = new List<String>();
+            // привязка к свойству Name
+            Label titleLabel = new Label { FontSize = 18 };
+            titleLabel.SetBinding(Label.TextProperty, "Title");
 
-            for (int i = 0; i < 50000; i++)
+            // привязка к свойству Company
+            Label companyLabel = new Label();
+            companyLabel.SetBinding(Label.TextProperty, "Company");
+
+            // привязка к свойству Price
+            Label priceLabel = new Label();
+            priceLabel.SetBinding(Label.TextProperty, "Price");
+
+            // создаем объект ViewCell.
+            return new ViewCell
             {
-                strings.Add("item number " + i);
-            }
+                View = new StackLayout
+                {
+                    Padding = new Thickness(0, 5),
+                    Orientation = StackOrientation.Vertical,
+                    Children = { titleLabel, companyLabel, priceLabel }
+                }
+            };
+        }
 
-            return strings;
+        private bool RunTimer = true;
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            RunTimer = false;
+
+        }
+        private async void PopulateListItems()
+        {
+            while (RunTimer)
+            {
+                Console.WriteLine(String.Format("add line {0}", phones.Count));
+                phones.Add(new Phone(String.Format("first {0}", phones.Count), "apple", 100 * phones.Count));
+                await Task.Delay(1000);
+            }
         }
     }
 }
---
+++ App1/Phone.cs
@@ -0,0 +1,16 @@
+﻿namespace App1
+{
+    public class Phone
+    {
+        public Phone(string title, string company, int price)
+        {
+            Title = title;
+            Company = company;
+            Price = price;
+        }
+
+        public string Title { get; }
+        public string Company { get; }
+        public int Price { get; }
+    }
+}
